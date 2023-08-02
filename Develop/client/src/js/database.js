a10f31dcf8649d0b1c5d357d541e7c37c78fc99d
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-08-01 21:33:54 -0400

    feature: logic for putDb and getDb

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,36 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('mute', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('mute')) {
+        console.log('mute database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('mute', { keyPath: 'id', autoIncrement: true });
+      console.log('mute database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const muteDb = await openDB('mute', 1);
+  const tx = muteDb.transaction('mute', 'readwrite');
+  const store = tx.objectStore('mute');
+  const request = store.add(content);
+  const result = await request;
+  console.log('Content added to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const muteDb = await openDB('mute', 1);
+  const tx = muteDb.transaction('mute', 'readonly');
+  const store = tx.objectStore('mute');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
