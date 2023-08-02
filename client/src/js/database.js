20d5a2cf7380b44acf60643654a34efbf763e579
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-08-02 18:10:02 -0400

    feature: updated 'putDb'; removed unnecessary comments; moved production code from 'Develop' folder to root

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,17 +12,15 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const muteDb = await openDB('mute', 1);
   const tx = muteDb.transaction('mute', 'readwrite');
   const store = tx.objectStore('mute');
-  const request = store.add(content);
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Content added to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const muteDb = await openDB('mute', 1);
   const tx = muteDb.transaction('mute', 'readonly');
@@ -30,7 +28,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
