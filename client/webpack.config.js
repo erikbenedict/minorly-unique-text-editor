1ecf31f7bf0bd9d53dd8d6ff629dc934ffc135ad
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-08-03 18:23:15 -0400

    bug: testing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
         title: 'Minorly Unique Text Editor',
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',
+        swSrc: '/src-sw.js',
         swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
